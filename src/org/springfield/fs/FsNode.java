6075c23a84a5202f4af628c73312fec4f808468d
Daniel Ockeloen <daniel@noterik.nl>
2014-07-20 16:28:33 +0200

    added methods for access checks

--- src/org/springfield/fs/FsNode.java
+++ src/org/springfield/fs/FsNode.java
@@ -18,10 +18,10 @@
 * You should have received a copy of the GNU General Public License
 * along with Lou.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 package org.springfield.fs;
 
 import java.util.*;
+
 import org.springfield.fs.*;
 
 //import org.springfield.lou.homer.LazyHomer;
@@ -194,6 +194,127 @@ public class FsNode implements Comparable<FsNode>  {
 	}
 	
 	
+	public ArrayList<String> allowedActions(String asker,String type) {
+		// access property looks like access_read = 'u(admin,daniel),a(euscreenpreview)'
+		// where u=user,a=application etc etc. Any normal writes on 'access_ are forbidden.
+		ArrayList<String> list = new ArrayList<String>();
+		
+		// we do them one by one to be sure
+		if (type.equals("user")) {
+			if (readAllowedForUser(asker)) list.add("read");
+			if (writeAllowedForUser(asker)) list.add("write");
+		} else if (type.equals("application")) {
+			if (readAllowedForAppliction(asker)) list.add("read");
+			if (writeAllowedForApplication(asker)) list.add("write");
+		}
+		return list;
+	}
+	
+	private boolean readAllowedForUser(String user) {
+		String accessline = this.getAccessProperty("read");
+		if (accessline!=null) {
+			int pos = accessline.indexOf("u(");
+			if (pos!=-1) {
+				String userline = accessline.substring(pos+2);
+				userline = userline.substring(0,userline.indexOf(")"));
+				String[] users = userline.split(",");
+				for (int i = 0; i<users.length;i++) {
+					if (users[i].equals(user)) return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private boolean writeAllowedForUser(String user) {
+		String accessline = this.getAccessProperty("write");
+		if (accessline!=null) {
+			int pos = accessline.indexOf("u(");
+			if (pos!=-1) {
+				String userline = accessline.substring(pos+2);
+				userline = userline.substring(0,userline.indexOf(")"));
+				String[] users = userline.split(",");
+				for (int i = 0; i<users.length;i++) {
+					if (users[i].equals(user)) return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private boolean readAllowedForAppliction(String app) {
+		String accessline = this.getAccessProperty("read");
+		if (accessline!=null) {
+			int pos = accessline.indexOf("a(");
+			if (pos!=-1) {
+				String appline = accessline.substring(pos+2);
+				appline = appline.substring(0,appline.indexOf(")"));
+				String[] apps = appline.split(",");
+				for (int i = 0; i<apps.length;i++) {
+					if (apps[i].equals(app)) return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private boolean writeAllowedForApplication(String app) {
+		String accessline = this.getAccessProperty("write");
+		if (accessline!=null) {
+			int pos = accessline.indexOf("a(");
+			if (pos!=-1) {
+				String appline = accessline.substring(pos+2);
+				appline = appline.substring(0,appline.indexOf(")"));
+				String[] apps = appline.split(",");
+				for (int i = 0; i<apps.length;i++) {
+					if (apps[i].equals(app)) return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	
+	public String getAccessProperty(String type) {
+		String path = this.getPath();
+		// /domain/
+		path = path.substring(8);
+		int pos = path.indexOf("/");
+		String dpath = "/domain/"+path.substring(0,pos);
+		path = path.substring(pos);
+		if (path.endsWith("/")) path = path.substring(0,path.length()-1); // remove last '/' if attached
+		
+		boolean finished = false;
+		FsNode node = null;
+		while (!finished) {
+			//System.out.println("TYPE="+type+" "+dpath+" "+path);
+			if (Fs.isMainNode(dpath+path)) {
+				//System.out.println("MAINNODE");
+				node = Fs.getNode(dpath+path+"/.access");
+			} else {
+				//System.out.println("SUBNODE");
+				node = Fs.getNode(dpath+path);	
+			}
+			
+			if (node!=null) {
+				//System.out.println("NODE="+node.asXML());
+				String result = node.getProperty("access_"+type);
+				if (result!=null) {
+					//System.out.println("RETURN = "+result);
+					return result;
+				}
+			}
+			
+			pos  = path.lastIndexOf("/");
+			if (pos!=-1) {
+				path = path.substring(0,pos);
+			} else {
+				finished = true;
+			}
+		}
+		return null;
+	}
+
 	
 	public String asXML(){
 		String xml = "<" + this.getName() + " id=\"" + this.getId() + "\"";
@@ -239,5 +360,11 @@ public class FsNode implements Comparable<FsNode>  {
 		}		
 		asindex = buf.toString();
 		return asindex;
+	}
+	
+	public boolean isMainNode() {
+		int r = path.split("/").length;
+		if  ( r % 2 == 0 ) return true;
+		return false;
 	}
 }
