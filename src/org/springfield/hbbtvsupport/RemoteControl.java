91c30166135036889fcc786c60d196b5d3417c09
Daniel Ockeloen <daniel@noterik.nl>
2014-08-16 16:52:23 +0200

    added support class for HbbTV

---
+++ src/org/springfield/hbbtvsupport/RemoteControl.java
@@ -0,0 +1,35 @@
+package org.springfield.hbbtvsupport;
+
+public class RemoteControl {
+	public final static int REMOTEKEY_RED = 403;
+	public final static int REMOTEKEY_GREEN = 404;
+	public final static int REMOTEKEY_YELLOW = 405;
+	public final static int REMOTEKEY_BLUE = 406;
+	
+	public final static int REMOTEKEY_LEFT = 37;
+	public final static int REMOTEKEY_UP = 38;
+	public final static int REMOTEKEY_RIGHT = 39;
+	public final static int REMOTEKEY_DOWN = 40;
+	public final static int REMOTEKEY_ENTER = 13;
+	
+	public final static int REMOTEKEY_0 = 48;
+	public final static int REMOTEKEY_1 = 49;
+	public final static int REMOTEKEY_2 = 50;
+	public final static int REMOTEKEY_3 = 51;
+	public final static int REMOTEKEY_4 = 52;
+	public final static int REMOTEKEY_5 = 53;
+	public final static int REMOTEKEY_6 = 54;
+	public final static int REMOTEKEY_7 = 55;
+	public final static int REMOTEKEY_8 = 56;
+	public final static int REMOTEKEY_9 = 57;
+	
+	
+	public final static int REMOTEKEY_PLAY = 415;
+	public final static int REMOTEKEY_PAUSE = 414;
+	public final static int REMOTEKEY_STOP = 413;
+	public final static int REMOTEKEY_FAST_FWD = 417;
+	public final static int REMOTEKEY_REWIND = 412;
+	public final static int REMOTEKEY_BACK = 461;
+	public final static int REMOTEKEY_TELETEXT = 459;
+	
+}
